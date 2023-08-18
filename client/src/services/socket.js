f592c8d9ea232b78327f334762d66a22e3137301
muhammad <muhammadaljoufi@gmail.com>
2023-08-18 15:50:48 +0500

    added package.json

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
-const socket = io("https://chat-app-bice-ten.vercel.app/");
+const socket = io("https://chat-app-bice-ten.vercel.app");
 
 export default socket;
